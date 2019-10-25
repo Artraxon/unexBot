f1df006d99f75f9986b1d751b87a93ee6c2cfaa5
Leonhard Rose <a@rtrx.de>
2019-10-25 18:06:26 +0200

    added the correct SQL Function for creating checks

--- src/main/kotlin/de/rtrx/a/database/DDL.kt
+++ src/main/kotlin/de/rtrx/a/database/DDL.kt
@@ -88,17 +88,23 @@ object SQLFunctions {
         """.trimIndent()
 
     val createCheck = """
-        create function comment_with_message(submission_id text, message_id text, comment_id text, message_body text, comment_body text, author_id text, comment_time timestamp with time zone, message_time timestamp with time zone)
-          returns void
+        create function create_check(submission_id text, tmptz timestamp with time zone, user_reports json, user_reports_dismissed json, is_deleted boolean, submission_score integer, removed_by text, flair text, current_sticky text, unexscore integer, top_posts_id text[], top_posts_score integer[])
+        returns void
         language plpgsql
         as $$
         DECLARE
         BEGIN
-          INSERT INTO comments VALUES (comment_id, comment_body, comment_time, reddit_username());
-          INSERT INTO relevant_messages VALUES (message_id, submission_id, message_body, author_id, message_time);
-          INSERT INTO comments_caused VALUES (message_id, comment_id);
-        END
-        $$;
+          INSERT INTO public."check" VALUES (${'$'}1, ${'$'}2, ${'$'}3, ${'$'}4, ${'$'}5, ${'$'}6, ${'$'}7, ${'$'}8, ${'$'}9);
+          INSERT INTO public.unex_score VALUES (${'$'}1, ${'$'}2, ${'$'}10);
+          FOR i in 1..10
+          LOOP
+            IF top_posts_id[i] IS NULL THEN
+              EXIT;
+            end if;
+            INSERT INTO public.top_posts VALUES (${'$'}1, ${'$'}2, top_posts_id[i], top_posts_score[i]);
+          end loop;
+          END;
+       $$;
         """.trimIndent()
 
     val redditUsername = """
