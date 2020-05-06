e4d5d2e9be8871e46e60c7fe71abea9cad27c83c
Leonhard Rose <a@rtrx.de>
2020-05-06 12:13:14 +0200

    Version Bump 2.1.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-project.version = "2.1.9"
+project.version = "2.1.10"
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.3.41"
 
@@ -32,6 +32,7 @@ dependencies {
     implementation(group= "org.slf4j", name= "slf4j-jdk14", version= "1.7.28")
 
     implementation(group="com.google.inject", name= "guice", version = "4.2.3")
+    implementation(group="com.google.inject.extensions", name= "guice-assistedinject", version = "4.2.3")
     testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
     testImplementation( "org.mockito:mockito-inline:2.13.0")
 
